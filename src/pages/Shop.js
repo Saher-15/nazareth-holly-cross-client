b27dd125b0845db0b2d54790d0959c527eb837e3
Saher15 <saher.saadi.99@gmail.com>
2024-04-30 19:33:19 +0300

    try to push

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Product } from "../components/Product";
 import { PRODUCTS } from "../Products";
 import "../styles/Shop.css";
+import axios from "axios";
+
 
 
 function Shop() {
+
+const [data, setData]=useState();
+
+  useEffect(() => {
+
+    async function getAllProduct() {
+      await axios.get(' https://nazareth-holly-city-server-8b53453baac6.herokuapp.com/product/getAllProducts').then(res => {
+        const data = res.data
+        console.log(res)
+        console.log(res.data)
+        setData(data)
+
+      })
+    }
+
+    getAllProduct();
+
+
+  }, [])
+
   return (
     <div className="shop">
-
-      <div className="products">
-        {PRODUCTS.map((product) => (
-          <Product data={product} />
-        ))}
-      </div>
 
       <div className="products">
         {PRODUCTS.map((product) => (
