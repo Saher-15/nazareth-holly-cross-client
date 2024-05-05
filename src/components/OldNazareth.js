42384b811c5ea4fe9fa95ebab85a4e7e20c32342
Saher15 <saher.saadi.99@gmail.com>
2024-05-06 00:17:57 +0300

    update

--- src/components/OldNazareth.js
+++ src/components/OldNazareth.js
@@ -1,3 +1,4 @@
+// OldNazareth.js
 import React from 'react';
 import '../styles/Pages.css';
 
@@ -5,15 +6,15 @@ function OldNazareth() {
   return (
     <div className='mypage'>
       <div className='header'>
-        <h1>Welcome to Nazareth</h1>
-        <p>Explore the beauty of Nazareth's Old City</p>
+        <h1 className='header-title'>Welcome to Nazareth</h1>
+        <p className='header-description'>Explore the beauty of Nazareth's Old City</p>
       </div>
       <div className='gallery'>
         <img src="/images/img-2.jpg" alt='Nazareth 1' className='gallery-image' />
         <img src="/images/img-1.jpg" alt='Nazareth 2' className='gallery-image' />
         <img src="/images/img-3.jpg" alt='Nazareth 3' className='gallery-image' />
       </div>
-      <p>
+      <p className='description'>
         Nazareth’s Old City is most famous for its shuk (outdoor market) which attracts Israelis from across the country looking for traditional Arabic produce. This in itself is an experience, and a great contrast to the air-conditioned malls dotted around the country. For those interested in Christianity, the Old City and surrounds are filled with important Christian sites, including the Church of the Annunciation.
       </p>
     </div>
