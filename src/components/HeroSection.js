f4de35dc3cabcf42373abd28c43ccb946f5afdf0
Saher15 <saher.saadi.99@gmail.com>
2024-05-13 13:49:38 +0300

    update

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import '../App.css';
 import { Button } from './Button';
 import '../styles/HeroSection.css';
 
 function HeroSection() {
   return (
     <div className='hero-container'>
-<video src='/videos/video-7.mp4' autoPlay loop muted playsInline controls />
+      <video src='/videos/video-7.mp4' autoPlay loop muted playsInline controls />
       <h1>JESUS CITY AWAITS</h1>
       <p>GRACE IN EVERY CLICK</p>
       <div className='hero-btns'>
@@ -18,14 +17,14 @@ function HeroSection() {
         >
           LIGHT A CANDLE
         </Button>
-        {/* <Button
+        <Button
           destination='/shop'
           className='btns'
           buttonStyle='btn--half'
           buttonSize='btn--large'
         >
-          SHOP
-        </Button> */}
+          SHOP <i className="fa fa-shopping-cart" aria-hidden="true"></i>
+        </Button>
         <Button
           destination='/tour'
           className='btns'
