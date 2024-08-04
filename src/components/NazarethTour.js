88ca2ad345b1c653048444b19c241947be476a68
Saher15 <saher.saadi.99@gmail.com>
2024-08-05 00:09:01 +0300

    update

--- src/components/NazarethTour.js
+++ src/components/NazarethTour.js
@@ -5,8 +5,9 @@ const NazarethTour = () => {
   return (
     <div className="container-tour">
       <p className="prayer-message">
-        Let's take a tour in Nazareth, a city brimming with history and culture. Discover vibrant markets, historic churches, and unique landmarks for an unforgettable experience.</p>
-      <video controls>
+        Let's take a tour in Nazareth, a city brimming with history and culture. Discover vibrant markets, historic churches, and unique landmarks for an unforgettable experience.
+      </p>
+      <video controls poster="/images/jesus_city_tour.png">
         <source src="/videos/tour.mp4" type="video/mp4" />
       </video>
     </div>
