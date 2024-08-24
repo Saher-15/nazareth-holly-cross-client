971aa5d4899de2d020d560a284f188c978fcdacf
Saher15 <saher.saadi.99@gmail.com>
2024-08-24 18:24:46 +0300

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,13 +2,14 @@ import React from 'react';
 import '../App.css';
 import Cards from '../components/Cards';
 import HeroSection from '../components/HeroSection';
+import LiveVideo from '../components/LiveVideo';
 
 function Home() {
   return (
     <>
       <HeroSection />
       <Cards />
-      
+      <LiveVideo />
 
     </>
   );
