937ccd717d3cc089f665c14fd2303a7edb171a29
Saher15 <saher.saadi.99@gmail.com>
2024-08-24 04:23:25 +0300

    update

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -34,7 +34,7 @@ function HeroSection() {
   return (
     <div className='hero-container'>
     
-      <video className='hero-v' src='/videos/video-7.mp4' autoPlay loop muted playsInline controls={false} />
+      <video id="video-field" className='hero-v' src='/videos/video-7.mp4' autoPlay loop muted></video>
       <h1>JESUS CITY AWAITS</h1>
       <p>GRACE IN EVERY CLICK</p>
       <button className='sound-control-btn' onClick={togglePlayPause}>
