d2b16c02a23cacd46fe41e7b982406b50c825755
Saher15 <saher.saadi.99@gmail.com>
2024-08-27 05:31:13 +0300

    update

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,11 +1,21 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { Button } from './Button';
+import { Link, useNavigate } from "react-router-dom";
 import '../styles/HeroSection.css';
 
 function HeroSection() {
   const [isPlaying, setIsPlaying] = useState(true);
   const audioRef = useRef(new Audio('/sounds/christians.mp3'));
+  const navigate = useNavigate();
+  const handleShopClick = () => {
+    // Reset filters before navigating
+    localStorage.removeItem('searchQuery');
+    localStorage.removeItem('sortOrder');
+    localStorage.setItem('currentPage', 1);
 
+    // Navigate to the shop page
+    navigate('/shop');
+  };
   useEffect(() => {
     // Scroll to the top of the page when the component mounts
     window.scrollTo(0, 0);
@@ -33,13 +43,13 @@ function HeroSection() {
 
   return (
     <div className='hero-container'>
-    
+
       <video id="video-field" className='hero-v' src='/videos/video-7.mp4' autoPlay loop muted playsInline controls={false} />
       <h1>JESUS CITY AWAITS</h1>
       <p>GRACE IN EVERY CLICK</p>
       <button className='sound-control-btn' onClick={togglePlayPause}>
-          <i className={isPlaying ? 'fas fa-volume-mute' : 'fas fa-volume-up'}></i>
-        </button>
+        <i className={isPlaying ? 'fas fa-volume-mute' : 'fas fa-volume-up'}></i>
+      </button>
       <div className='hero-btns'>
         <Button
           destination='/candle'
@@ -49,14 +59,24 @@ function HeroSection() {
         >
           LIGHT A CANDLE
         </Button>
-        <Button
+        {/* <Button
           destination='/shop'
           className='btns'
           buttonStyle='btn--half'
           buttonSize='btn--large'
         >
           SHOP <i className="fa fa-shopping-cart" aria-hidden="true"></i>
-        </Button>
+        </Button> */}
+        <Link
+          to="/shop"
+          onClick={handleShopClick}
+        >
+          <Button className='btns'
+            buttonStyle='btn--half'
+            buttonSize='btn--large'>
+            Shop<i className="fa fa-shopping-cart" aria-hidden="true"></i>
+          </Button>
+        </Link>
         <Button
           destination='/tour'
           className='btns'
