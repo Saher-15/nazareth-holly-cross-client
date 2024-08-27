97ff7983e720910f5bd0153b16bdee7c1d17c069
Saher15 <saher.saadi.99@gmail.com>
2024-08-27 05:11:17 +0300

    update

--- src/pages/cart/cart.js
+++ src/pages/cart/cart.js
@@ -9,9 +9,13 @@ const Cart = () => {
 
   const navigate = useNavigate();
 
-  const [totalAmount, setTotalAmount] = useState();
+  const [totalAmount, setTotalAmount] = useState(0); // Initialize totalAmount to 0
 
   useEffect(() => {
+    // Scroll to the top of the page when the component mounts
+    window.scrollTo(0, 0);
+
+    // Calculate the total amount
     function calcTotalAmount() {
       let total = 0;
       cartItems.forEach(item => {
@@ -28,7 +32,7 @@ const Cart = () => {
       <div>
         <h1>Your Cart Items</h1>
       </div>
-      <div className="cart">
+      <div className="cart-items">
         {cartItems.map((product) => (
           <CartItem
             key={product._id}
@@ -40,15 +44,14 @@ const Cart = () => {
 
       {totalAmount > 0 ? (
         <div className="checkout">
-          <p> Subtotal: ${totalAmount} </p>
-          <button onClick={() => navigate("/shop")}> Continue Shopping </button>
-          <button onClick={() => navigate("/checkout", { state: { totalAmount: totalAmount, cartItems: cartItems } })}> Checkout </button>
+          <p>Subtotal: ${totalAmount.toFixed(2)}</p> {/* Format totalAmount */}
+          <button onClick={() => navigate("/shop")}>Continue Shopping</button>
+          <button onClick={() => navigate("/checkout", { state: { totalAmount, cartItems } })}>Checkout</button>
         </div>
       ) : (
         <div className="checkout">
-        
-          <h1> Your Shopping Cart is Empty</h1>
-          <button onClick={() => navigate("/shop")}> Back To Shopping </button>
+          <h1>Your Shopping Cart is Empty</h1>
+          <button onClick={() => navigate("/shop")}>Back To Shopping</button>
         </div>
       )}
     </div>
