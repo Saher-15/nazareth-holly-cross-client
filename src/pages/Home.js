0e42db56023466dd54efaf048bd5018cc0fa496f
Saher15 <saher.saadi.99@gmail.com>
2024-09-04 23:45:43 +0300

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../App.css';
 import Cards from '../components/Cards';
 import HeroSection from '../components/HeroSection';
 import WhatIsNew from '../components/WhatIsNew';
+import ReactGA from 'react-ga';
+
+const trackingId = "G-VE42K6WP4H"; // Replace with your Google Analytics tracking ID
+ReactGA.initialize(trackingId);
 
 function Home() {
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
   return (
     <>
       <HeroSection />
       <Cards />
       <WhatIsNew />
-
     </>
   );
 }
